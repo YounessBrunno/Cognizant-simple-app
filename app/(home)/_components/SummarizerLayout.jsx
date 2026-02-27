99dfd0aa0c51e1ed0b08837bf1a2936afeae1045
youness oukhadjou <brunnotools@gmail.com>
2026-02-27 14:35:57 +0000

    feat: add SummarizerLayout component and new summarizer feature components

---
+++ app/(home)/_components/SummarizerLayout.jsx
@@ -0,0 +1,5 @@
+export default function SummarizerLayout({ children }) {
+  return (
+    <div>{children}</div>
+  )
+}
---
+++ features/summarizer/components/SummaryResult.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SummaryResult() {
+  return (
+    <div>SummaryResult</div>
+  )
+}
