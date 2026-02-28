fbd63729902ca5548c711add5699f9d287b71916
youness oukhadjou <brunnotools@gmail.com>
2026-02-28 15:58:16 +0000

    feat: implement GET and POST handlers in summarize route

---
+++ app/api/summarize/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest } from "next/server";
+
+export async function GET(req : NextRequest) {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+export async function POST(req : NextRequest) {
+  try {
+    
+  } catch (error) {
+    
+  }  
+}
