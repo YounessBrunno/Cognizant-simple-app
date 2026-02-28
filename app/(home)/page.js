3b4b33d418e5b01c7dfd31fdb354ee1c12f7da56
youness oukhadjou <brunnotools@gmail.com>
2026-02-28 14:39:28 +0000

    feat: enhance SummarizerForm styling and replace icon with CornerDownLeftIcon

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <div className="text-center">
-        <h1 className="text-4xl font-bold font-mono text-black">Welcome to Cognizant App</h1>
+        <h1 className="text-4xl font-bold font-mono text-black mb-8">Welcome to Cognizant App</h1>
       </div>
       <SummarizerForm />
     </>
--- features/summarizer/components/SummarizerForm.jsx
+++ features/summarizer/components/SummarizerForm.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from 'react';
-import { FileTextIcon } from 'lucide-react';
+import { CornerDownLeftIcon} from 'lucide-react';
 
 export default function SummarizerForm() {
   const [input, setInput] = useState('');
@@ -22,17 +22,18 @@ export default function SummarizerForm() {
 
   return (
       <div className="w-full max-w-2xl">
-        <div className="flex items-center bg-white border-2 border-gray-300 rounded-3xl shadow-sm focus-within:shadow-md transition-shadow">
+        <div className="relative flex items-center bg-white border-2 border-gray-300 rounded-3xl shadow-sm focus-within:shadow-md transition-shadow">
           <textarea
             value={input}
             onChange={(e) => setInput(e.target.value)}
             placeholder="Enter text to summarize..."
-            className="relative flex-1 px-6 py-4 bg-white text-black placeholder-gray-400 outline-none resize-none max-h-32"
+            className="flex-1 px-6 py-4 rounded-3xl bg-white text-black placeholder-gray-400 outline-none resize-none max-h-32"
             rows="3"
           />
-          <button onClick={handleSummarize}
-            className="absolute mr-2 px-4 py-2 bg-black text-white font-bold rounded-xl hover:bg-gray-800 transition-colors">
-            <FileTextIcon className="w-5 h-5" />
+          <button 
+            onClick={handleSummarize}
+            className="absolute bottom-1 right-1 px-3 py-1.5 bg-black border-2 border-gray-300 text-white font-bold rounded-full hover:bg-gray-800 transition-colors">
+            <CornerDownLeftIcon className="w-5 h-5" />
           </button>
         </div>
       </div>
