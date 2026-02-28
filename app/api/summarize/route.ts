a28bc94850fd0fe83f2b4ca72f90e7aa40f80b0c
youness oukhadjou <brunnotools@gmail.com>
2026-02-28 23:43:49 +0000

    feat: implement Hugging Face inference client and update summarize route

--- app/api/summarize/route.ts
+++ app/api/summarize/route.ts
@@ -1,13 +1,21 @@
 import { NextRequest } from "next/server";
+import { hf } from "@/config/inference.huggingface";
 
-export async function GET(req : NextRequest) {
+export async function POST(req : NextRequest) {
   try {
-    
+      const body = await req.json();
+
+      const res = await hf.summarization({
+        model: "google/flan-t5-small",
+        inputs: body.text,
+      })
+      return new Response(JSON.stringify(res), { status: 200 });
   } catch (error) {
-    
+    return new Response("Error processing request", { status: 500 });
   }
 }
-export async function POST(req : NextRequest) {
+
+export async function GET(req : NextRequest) {
   try {
     
   } catch (error) {
