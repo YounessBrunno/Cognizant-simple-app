34bc7013b733117057d69e32371aadc98a37075b
youness oukhadjou <brunnotools@gmail.com>
2026-03-01 17:58:00 +0000

    feat: added copy button with styling on copy for better UX

--- features/summarizer/components/SummaryResult.jsx
+++ features/summarizer/components/SummaryResult.jsx
@@ -1,18 +1,26 @@
-
+import { CopyIcon } from "lucide-react";
 
 export default function SummaryResult({ summary, error }) {
   return (
     <div>   
      {summary && 
        (
-           <div className="mt-4 p-4 bg-gray-100 rounded-lg">
+           <div className="relative mt-4 p-4 bg-gray-100 rounded-lg border border-gray-300">
+             <button className="absolute top-1.5 right-1.5 p-1.5 rounded-md cursor-pointer hover:bg-gray-200 transition">
+               <CopyIcon 
+                 className="w-5 h-5 text-gray-500 hover:text-gray-700"
+                 strokeWidth={1.5}
+                 onClick={() => navigator.clipboard.writeText(summary)}
+               />
+             </button>
+
              <h3 className="font-bold mb-2">Summary:</h3>
              <p className="text-gray-700">{summary}</p>
            </div>
      )}
 
      {error && (
-           <div className="mt-4 p-4 bg-red-100 text-red-700 rounded-lg">
+           <div className="mt-4 p-3 bg-red-100 text-red-700 rounded-lg">
              Error: {error}
            </div>
      )}
