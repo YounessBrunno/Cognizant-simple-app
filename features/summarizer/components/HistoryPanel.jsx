eb5653fe262471ecc52aaabdfb792789b74f2780
youness oukhadjou <brunnotools@gmail.com>
2026-03-01 19:55:39 +0000

    feat: went with localStorage for simplicity, added HistoryPanel component and add Clear History button

--- features/summarizer/components/HistoryPanel.jsx
+++ features/summarizer/components/HistoryPanel.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
-function HistoryPanel() {
+export default function HistoryPanel() {
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">History</h2>
+      <button className="w-full bg-black text-white py-2 px-9 rounded-md hover:bg-black/20 transition">Clear History</button>
     </div>
   )
 }
 
-export default HistoryPanel
